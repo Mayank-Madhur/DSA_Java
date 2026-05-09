b2881c18aa20d7bf8033fdc4597b6a25328f62be
Mayank <madhurmayank43@gmail.com>
2026-05-09 14:45:07 +0530

    inheritance

--- Student.java
+++ Student.java
@@ -1,27 +1,18 @@
-package Lec22;
+package Lec24;
 
 public class Student {
 
 	String name;
 	int age;
 
-	public void Intro_yourSelf() {
-		System.out.println("My name is " + this.name + " and age is " + this.age);
+	public Student(String name, int age) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return name+" "+age;
 	}
 
-	public void SayHey(String name) {
-
-		System.out.println(this.name + " Say Hey " + name);
-	}
-
-	public static void MentorName(Student s) {
-		s.Intro_yourSelf();
-		System.out.println("Monu Bhaiya" + " "+s.name);
-	}
-	static {
-		System.out.println("Student Class Block1 ");
-	}
-	static {
-		System.out.println("Student Class Block2 ");
-	}
 }
---
+++ Student_Client.java
@@ -0,0 +1,14 @@
+package Lec24;
+
+public class Student_Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student("Raj", 22);
+		System.out.println(s);//toString()
+//		Object
+		String s1="Kunal";
+		System.out.println(s1);
+	}
+
+}
