284f437d7b72ce90354ca343c32209dc5e0ddc27
Mayank <madhurmayank43@gmail.com>
2026-05-09 14:57:23 +0530

    LinkedList

---
+++ Cycle_removal.java
@@ -0,0 +1,124 @@
+package Lec28;
+
+public class Cycle_removal {
+
+	private class Node {
+		int val;
+		Node next;
+
+	}
+
+	private Node head;
+	private Node tail;
+	private int size;
+
+	// O(1)
+	public void addLast(int item) {
+		Node nn = new Node();
+		nn.val = item;
+		if (size == 0) {
+			head = nn;
+			tail = nn;
+			size++;
+		} else {
+			tail.next = nn;
+			tail = nn;
+			size++;
+		}
+	}
+
+	private Node getNode(int k) {
+
+		Node temp = head;
+		for (int i = 0; i < k; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+	public void CreateCycle() {
+		tail.next = getNode(2);
+	}
+
+	public void Display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.val + "-->");
+			temp = temp.next;
+		}
+		System.out.println(".");
+
+	}
+
+	public Node hasCycle() {
+		Node slow = head;
+		Node fast = head;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (slow == fast) {
+				return slow;
+			}
+
+		}
+		return null;
+	}
+	public void Cycle_removal() {
+		Node meet = hasCycle();
+		if (meet ==null) {
+			return;
+		}
+		Node start = head;
+		while (start != null) {
+			Node temp = meet;
+			while(temp.next != meet) {
+				if(temp.next == start) {
+					temp.next = null;
+					return;
+				}
+				temp = temp.next;
+			}
+			start = start.next;
+		}
+	}
+	public void Cycle_removal2() {
+		Node meet = hasCycle();
+		if (meet == null) {
+			return;
+		}
+		//cycle ka length
+		int m = 1;
+		Node temp = meet;
+		while (temp.next != meet) {
+			m++;
+			temp = temp.next;
+		}
+		//ek node ko m distance pehle move kra do
+		Node fast = head;
+		for (int i = 0; i < m; i++) {
+			fast = fast.next;
+		}
+		// ||both node ko same speed se move krana hai
+		Node slow = head;
+		while(slow.next != fast.next) {
+		slow = slow.next;
+		fast = fast.next;
+		}
+		fast.next=null;
+	}
+	public static void main(String[] args) throws Exception {
+		Cycle_removal cl = new Cycle_removal();
+		cl.addLast(1);
+		cl.addLast(2);
+		cl.addLast(3);
+		cl.addLast(4);
+		cl.addLast(5);
+		cl.addLast(6);
+		cl.addLast(7);
+		cl.addLast(8);
+		cl.CreateCycle();
+		cl.Cycle_removal();
+		cl.Display();
+	}
+
+}
---
+++ Reverse_Nodes_in_k_Group.java
@@ -0,0 +1,56 @@
+package Lec28;
+import java.util.Stack;
+public class Reverse_Nodes_in_k_Group {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	class Solution {
+		public ListNode reverseKGroup(ListNode head, int k) {
+			Stack<ListNode> st = new Stack<>();
+			ListNode dummy = new ListNode();
+			ListNode temp = dummy;
+			ListNode prev = head;
+			while (head != null) {
+				// stack me k element add krna
+				int c = 0;
+				while (head != null && c < k) {
+					st.push(head);
+					head = head.next;
+					c++;
+				}
+
+				// reverse attach krna
+				if (c == k) {
+					while (!st.isEmpty()) {
+						dummy.next = st.pop();
+						dummy = dummy.next;
+					}
+					  dummy.next=null;
+				}
+				else {
+					dummy.next=prev;
+				}
+				prev = head;
+
+			}
+			return temp.next;
+
+		}
+	}
+
+}
