a21e0f8df985ce97e9179c1a365203c7928f3c69
Mayank <madhurmayank43@gmail.com>
2026-05-12 14:19:38 +0530

    Bit-Masking

---
+++ Left_Shift.java
@@ -0,0 +1,12 @@
+package Lec45;
+
+public class Left_Shift {
+	public static void main(String[] args) {
+		int n = 17;
+		n <<= 2;// n = n<<2
+		int n1 = -17;
+		n1 <<= 2;// n1 = n1<<2
+		System.out.println(n);
+		System.out.println(n1);
+	}
+}
---
+++ Right_Shift.java
@@ -0,0 +1,15 @@
+ package Lec45;
+
+public class Right_Shift {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 17;
+		a >>= 1;
+		int b = -20;
+		b >>= 3;
+		System.out.println(a);// 8 
+		System.out.println(b);// -9
+	}
+
+}
